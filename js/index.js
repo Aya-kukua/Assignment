7ea4a97d51616d304ba3ba3314e020b4a823e7fe
Aya-Kukua <ayakukua001@gmail.com>
2025-05-15 09:53:09 +0000

    first draft

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.
+    if (window.scrollY > 70) {
+        // change navbar bg to blue
+        document.getElementById('navbar').style.backgroundColor ='#0779E4';
+    } else {
+        // change navbar bg to transparent
+         document.getElementById('navbar').style.backgroundColor ='transparent';}
